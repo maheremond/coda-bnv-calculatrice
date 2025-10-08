a7d694b7a3bad82e987850b956381fac0a0c1b72
mahe <maheremond91@gmail.com>
2025-10-08 12:05:44 +0000

    ajout création des conditio

--- mainprojet.c
+++ mainprojet.c
@@ -4,7 +4,7 @@ main()
 {
     printf("Bienvenue dans la calculatrice !\n");
     int operateur
-    printf "Choisissez un opérateur (+, -, *, /) :\n"
+    printf "Choisissez un opérateur (+, -, *, /, %%) :\n"
     scanf("%d", &operateur);
     int nb1
     int nb2
@@ -12,5 +12,24 @@ main()
     scanf("%d", &nb1);
     printf("Entrez un autre nombre :\n");
     scanf("%d", &nb2);
+        if (operateur == '+') {
+        printf("Résultat : %d\n", nb1 + nb2);
+    } else if (operateur == '-') {
+        printf("Résultat : %d\n", nb1 - nb2);
+    } else if (operateur == '*') {
+        printf("Résultat : %d\n", nb1 * nb2);
+    } else if (operateur == '/') {
+        if (nb2 != 0) {
+            printf("Résultat : %.2f\n", (float)nb1 / nb2);
+        } else {
+            printf("Erreur : Division par zéro !\n");
+            exit(1);
+        }
+    } else if (operateur == '%%') {
+        printf("Résultat : %d\n", nb1 % nb2);
+    } else {
+        printf("Erreur : Opérateur inconnu !\n");
+        exit(1);
+    }
     exit(0);
 }
