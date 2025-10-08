dc0edb93478b3889d990b24929a2ce62781b9441
mahe <maheremond91@gmail.com>
2025-10-08 11:43:12 +0000

    ajout des variable

--- mainprojet.c
+++ mainprojet.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 main()
 {
-    
+    int operateur
+    printf "Choisissez un opérateur (+, -, *, /) :\n"
+    scanf("%d", &operateur);
+    int nb1
+    int nb2
+    printf("Entrez un nombre :\n");
+    printf("Entrez un autre nombre :\n");
+    scanf("%d", &nb1);
+    scanf("%d", &nb2);
     exit(0);
 }
