f2553aa888247d89e51d38f910eebfcb7d030ff9
mahe <maheremond91@gmail.com>
2025-10-08 12:24:35 +0000

    corection bug

--- mainprojet.c
+++ mainprojet.c
@@ -1,35 +1,60 @@
-#include <stdlib.h>
 #include <stdio.h>
-int main()
-{
+#include <stdlib.h>
+
+int main(void) {
     printf("Bienvenue dans la calculatrice !\n");
-    int operateur;
-    printf("Choisissez un opérateur (+, -, , /, %%) :\n");
-    scanf("%d", &operateur);
-    int nb1;
-    int nb2;
-    printf("Entrez un nombre :\n");
-    scanf("%d", &nb1);
-    printf("Entrez un autre nombre :\n");
-    scanf("%d", &nb2);
-        if (operateur == '+') {
-        printf("Résultat : %d\n", nb1 + nb2);
-    } else if (operateur == '-') {
-        printf("Résultat : %d\n", nb1 - nb2);
-    } else if (operateur == '') {
-        printf("Résultat : %d\n", nb1 * nb2);
-    } else if (operateur == '/') {
-        if (nb2 != 0) {
-            printf("Résultat : %.2f\n", (float)nb1 / nb2);
-        } else {
-            printf("Erreur : Division par zéro !\n");
-            exit(1);
-        }
-    } else if (operateur == '%%') {
-        printf("Résultat : %d\n", nb1 % nb2);
-    } else {
-        printf("Erreur : Opérateur inconnu !\n");
-        exit(1);
+
+    char operateur;
+    int nb1, nb2;
+
+    printf("Choisissez un operateur (+, -, *, /, %%):\n");
+    if (scanf(" %c", &operateur) != 1) { // espace avant %c pour consommer les sauts de ligne
+        printf("Erreur : Lecture de l'operateur.\n");
+        return 1;
     }
-    exit(0);
+
+    printf("Entrez un nombre :\n");
+    if (scanf("%d", &nb1) != 1) {
+        printf("Erreur : Lecture du premier nombre.\n");
+        return 1;
+    }
+
+    printf("Entrez un autre nombre :\n");
+    if (scanf("%d", &nb2) != 1) {
+        printf("Erreur : Lecture du second nombre.\n");
+        return 1;
+    }
+
+    switch (operateur) {
+        case '+':
+            printf("Resultat : %d\n", nb1 + nb2);
+            break;
+        case '-':
+            printf("Resultat : %d\n", nb1 - nb2);
+            break;
+        case '*':
+            printf("Resultat : %d\n", nb1 * nb2);
+            break;
+        case '/':
+            if (nb2 != 0) {
+                printf("Resultat : %.2f\n", (float) nb1 / nb2);
+            } else {
+                printf("Erreur : Division par zero !\n");
+                return 1;
+            }
+            break;
+        case '%':
+            if (nb2 != 0) {
+                printf("Resultat : %d\n", nb1 % nb2);
+            } else {
+                printf("Erreur : Modulo par zero !\n");
+                return 1;
+            }
+            break;
+        default:
+            printf("Erreur : Operateur inconnu !\n");
+            return 1;
+    }
+
+    return 0;
 }
