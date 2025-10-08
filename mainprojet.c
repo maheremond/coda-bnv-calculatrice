24cddb65f43b398d969c8dfe01eaeea09c96580f
mahe <maheremond91@gmail.com>
2025-10-08 11:48:43 +0000

    ajout recuperation des information

--- mainprojet.c
+++ mainprojet.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 main()
 {
+    printf("Bienvenue dans la calculatrice !\n");
     int operateur
     printf "Choisissez un opérateur (+, -, *, /) :\n"
     scanf("%d", &operateur);
     int nb1
     int nb2
     printf("Entrez un nombre :\n");
-    printf("Entrez un autre nombre :\n");
     scanf("%d", &nb1);
+    printf("Entrez un autre nombre :\n");
     scanf("%d", &nb2);
     exit(0);
 }
